6539f25fecc6a6d6ce83ebb97ef0cc864a161fe5
hmralavi <hmralavi@gmail.com>
2024-04-18 15:20:42 +0330

    trying to fix github cronjob error: "403 Client Error: Forbidden for url: https://bonbast.com/"
    solution: make more attempts to get bonbast data

--- data_submitter/main.py
+++ data_submitter/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import subprocess
 from typing import List
+import time
 
 sys.path.append("src")
 
@@ -35,21 +36,31 @@ def post_data(url: str, data: List[PriceData]):
 
 
 def main():
-    urls = [
+    nerkh_server_urls = [
         "https://nerkh-api.liara.run/submit_prices",
         "https://nerkh-api-dev.liara.run/submit_prices",
         # "http://localhost:8000/submit_prices",
     ]
-    bonbast_prices = get_bonbast_prices()
+    attempts = 1
+    while attempts < 1001:
+        try:
+            print(f"attempt {attempts} to get bonbast data.")
+            bonbast_prices = get_bonbast_prices()
+            print("bonbast data received successfully.")
+            break
+        except Exception as e:
+            print(f"attempt {attempts} failed. error: {e}")
+            time.sleep(1)
+            attempts += 1
     translate_prices(bonbast_prices)
-    for url in urls:
+    for url in nerkh_server_urls:
         # Post the data to the URL
         response = post_data(url, bonbast_prices)
         # Check the response
         if response.status_code == 200:
-            print(f"Data posted successfully to {url}! response: {response.text}")
+            print(f"Data posted successfully to {url}. Status code: {response.status_code}, response: {response.text}")
         else:
-            print(f"Failed to post data to {url}. Status code: {response.status_code}, Error: {response.text}")
+            print(f"Failed to post data to {url}. Status code: {response.status_code}, response: {response.text}")
 
 
 if __name__ == "__main__":
